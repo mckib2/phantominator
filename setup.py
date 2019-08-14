fdc9c728b30324a8389a3569e3e8926f9812b279
mckib2 <nicholas.bgp@gmail.com>
2019-08-14 15:28:36 -0600

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='phantominator',
-    version='0.1.2',
+    version='0.1.3',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
