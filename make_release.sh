7b22ac93845a730c005368060c2ba37e51502d1d
mckib2 <nicholas.bgp@gmail.com>
2019-08-21 18:57:14 -0600

    Update release script for virtualenv compatibility.

--- make_release.sh
+++ make_release.sh
@@ -3,9 +3,9 @@
 rm -rf dist
 
 # Generate distribution archives
-python3 -m pip install --user --upgrade setuptools wheel
-python3 setup.py sdist bdist_wheel
+pip install --upgrade setuptools wheel
+python setup.py sdist bdist_wheel
 
 # Upload
-python3 -m pip install --user --upgrade twine
-python3 -m twine upload dist/*
+pip install --upgrade twine
+python -m twine upload dist/*
