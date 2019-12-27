e201f8165a60e8d120a854c40d06bf10babf5a6a
mckib2 <nicholas.bgp@gmail.com>
2019-12-26 18:47:51 -0700

    update numpy dependencies and bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='phantominator',
-    version='0.4.5',
+    version='0.5.0',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
@@ -15,8 +15,8 @@ setup(
     description='Generate numerical phantoms.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "numpy>=1.16.1",
-        "scipy>=1.3.1",
+        "numpy>=1.17.4",
+        "scipy>=1.3.3",
         "matplotlib>=2.1.1"
     ],
     python_requires='>=2.7', # 2.7.15+, to be exact...
