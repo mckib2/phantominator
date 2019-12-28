1185fba6b436108e40cf40ba06382f3cf91fb64a
mckib2 <nicholas.bgp@gmail.com>
2019-12-27 20:46:54 -0700

    view mr phantom images

--- examples/mr_shepp_logan.py
+++ examples/mr_shepp_logan.py
@@ -1,5 +1,7 @@
 '''Demonstrate how to use MR Shepp-Logan.'''
 
+import matplotlib.pyplot as plt
+
 from phantominator import shepp_logan
 
 if __name__ == '__main__':
@@ -7,4 +9,23 @@ if __name__ == '__main__':
     # Get proton density, T1, and T2 maps for (L, M, N) sized Shepp-
     # Logan phantom.
     L, M, N = 256, 255, 20
-    M0, T1, T2 = shepp_logan((L, M, N), MR=True)
+    M0, T1, T2 = shepp_logan((L, M, N), MR=True, zlims=(-.25, .25))
+
+    # Take a look
+    nx, ny = 2, 2
+    plt.subplot(nx, ny, 1)
+    plt.imshow(M0[..., 0])
+    plt.title('Proton Density')
+    plt.axis('off')
+
+    plt.subplot(nx, ny, 2)
+    plt.imshow(T1[..., 0])
+    plt.title('T1')
+    plt.axis('off')
+
+    plt.subplot(nx, ny, 3)
+    plt.imshow(T2[..., 0])
+    plt.title('T2')
+    plt.axis('off')
+
+    plt.show()
