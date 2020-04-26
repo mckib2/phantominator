cdcaf7113af2bd46c0da498d7aec51d72731e13e
mckib2 <nicholas.bgp@gmail.com>
2020-04-25 19:43:31 -0600

    Downgrade requirements for python 3.5 support

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages
 
 setup(
     name='phantominator',
-    version='0.6.1',
+    version='0.6.2',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
@@ -17,8 +17,8 @@ setup(
     install_requires=[
         "numpy>=1.18.0",
         "scipy>=1.4.1",
-        "matplotlib>=3.1.2",
-        "ssfp>=0.3.0",
+        "matplotlib>=3.0.0",
+        "ssfp>=0.4.2",
     ],
-    python_requires='>=3',
+    python_requires='>=3.5',
 )
